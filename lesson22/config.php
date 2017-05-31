d01bd8812409a57a3aa348cc39ec557bc5f6e1de
artem <lanart@ex.ua>
2017-05-31 22:21:48 +0300

    first commit

---
+++ lesson22/config.php
@@ -0,0 +1,11 @@
+<?php
+    class Config2 {
+        function __construct() {
+            $this->APP_PATH = dirname(dirname(__FILE__)) ;
+        }
+        protected $APP_PATH ;
+        protected $DB_HOST    = "localhost" ;
+        protected $DB_USER    = "root" ;
+        protected $DB_PASS    = "Creation" ;
+        protected $DB_NAME    = "easycode" ;
+    }
